b230f553881c5b260b0ffeadd5947bd1186e00fc
akhano014 <sarsarahtishmmughal@gmail.com>
2026-02-12 20:42:30 +0500

    Condintional Renderning

--- userGreeting/userGreeting.jsx
+++ userGreeting/userGreeting.jsx
@@ -1,7 +1,20 @@
 import Styles from "./userGreeting.module.css"
+import PropTypes from "prop-types"
 
 function UserGreeting(props){
-    return(props.isLoggedin ? <h1 className={Styles.welcomemsg}>Welcome {props.username} </h1> : <h2 className={Styles.tryagainmsg}>Please Try again Thank you</h2>);
+    const welcomemsg = <h1 className={Styles.welcomemsg}>Welcome {props.username} </h1> 
+    const tryagianmsg= <h2 className={Styles.tryagainmsg}>Please Try again Thank you</h2>
+
+    return(props.isLoggedin ? welcomemsg: tryagianmsg );
 }
 
+UserGreeting.PropTypes{
+    welcomemsg:PropTypes.bool
+    tryagainmsg:PropTypes.string
+}
+
+UserGreeting.defaultProps{
+    isLoggedin:false
+    username:"Guest"
+}
 export default UserGreeting 
